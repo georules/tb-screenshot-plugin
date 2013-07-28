98e8d5145055274e8be94e3245241f60662f286b
Geo Miller <geoffery.miller@gmail.com>
2013-07-27 21:53:00 -0400

    updating to include enjalot's changes in main trib repo

--- index.js
+++ index.js
@@ -9,27 +9,51 @@ function tributaryScreenshotPlugin(tributary, plugin) {
     // get the screenshot button and put it on the config panel
     d3.select("#thumbnail-content").node().appendChild(d3.select(el).select("#screenshot").node());
     d3.select("#screenshot").on("click", handleScreenshot);
-    var link = tributary.model.get("thumbnail");
-    // Update the thumbnail in the config panel
-    if (link) {
-      d3.select("#thumbnail-content").select("img").attr("src", link).style("display", "");
-    }
   }
   plugin.deactivate = function()  {
     el = document.getElementById(plugin.elId);
     el.innerHTML = "";
   }
 
+  tributary._screenshot = handleScreenshot;
   function handleScreenshot() {
-    svg = d3.select("#display").html();
-    findstring = '<svg xmlns="http://www.w3.org/2000/svg" xlink:xlink="http://www.w3.org/1999/xlink" class="tributary_svg" width="100%" height="100%">';
-    svg2 = svg.replace(findstring,'<svg>'); // hacky.
+    var display = tributary.__config__.get("display");
+    if(display === "svg") {
+      svgScreenshot();
+    } else if(display === "canvas" || display === "webgl") {
+      canvasScreenshot();
+    } else if(display === "div") {
+      htmlScreenshot();
+    }
+  }
+  function svgScreenshot() {
+    //svg = d3.select("#display").html();
+    var svg = new XMLSerializer().serializeToString(d3.select(".tributary_svg").node());
+    //var findstring = '<svg xmlns="http://www.w3.org/2000/svg" xlink:xlink="http://www.w3.org/1999/xlink" class="tributary_svg" width="100%" height="100%">';
+    //svg = svg.replace(findstring,''); // hacky.
+    svg = svg.replace(/^\<svg xmlns.+tributary_svg.+?>/,'<svg>'); // hacky.
+    
     canvas = document.getElementById('pngit');
-    canvg(canvas, svg2, {renderCallback: function() {
+    canvg(canvas, svg, {renderCallback: function() {
       var len = "data:image/png;base64,".length;
       var img = canvas.toDataURL("image/png").substring(len);
       tributary.events.trigger("imgur", img);
     }});
+  }
+  function canvasScreenshot() {
+    canvas = d3.select("#display canvas").node()
+    var len = "data:image/png;base64,".length;
+    var img = canvas.toDataURL("image/png").substring(len);
+    tributary.events.trigger("imgur", img);
+  }
+  function htmlScreenshot() {
+    html2canvas( [ d3.select("#display").node() ], {
+      onrendered: function(canvas) {
+        var len = "data:image/png;base64,".length;
+        var img = canvas.toDataURL("image/png").substring(len);
+        tributary.events.trigger("imgur", img);
+    }
+    });
   }
 
   return plugin;
